f0fa82e349d2f36efb4069e2e5ba22af595d5e59
Elaidzha1940 <eli080293@gmail.com>
2023-10-02 18:11:17 +0300

    Updated UNTimeIntervalNotificationTrigger

--- LocalPush Notifications/ContentView.swift
+++ LocalPush Notifications/ContentView.swift
@@ -25,19 +25,46 @@ class NotificationManager {
             }
         }
     }
+    
+    func schedulNotification () {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Notification"
+        content.subtitle = "It was easy "
+        content.sound = .default
+        content.badge = 1
+        
+        // MARK: Time
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
+        
+        //MARK: Calendar
+        
+        //MARK: Location
+        
+        let request = UNNotificationRequest(
+            identifier: UUID().uuidString,
+            content: content,
+            trigger: trigger)
+        UNUserNotificationCenter.current().add(request)
+    }
 }
 
 struct ContentView: View {
-
+    
     var body: some View {
         
         VStack {
-    
+            
             Button("Request ermission") {
                 NotificationManager.instance.requestAuthorization()
             }
+            
+            Button("S chedul Notification") {
+                NotificationManager.instance.requestAuthorization()
+            }
+
         }
-       // .preferredColorScheme(.dark)
+        .preferredColorScheme(.dark)
     }
 }
 
