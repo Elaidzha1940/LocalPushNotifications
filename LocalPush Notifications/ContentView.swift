5bcf51ac3e3b2fa4af409e55a9e08710d7f26046
Elaidzha1940 <eli080293@gmail.com>
2023-10-02 05:09:43 +0300

    imported UserNotifications

--- LocalPush Notifications/ContentView.swift
+++ LocalPush Notifications/ContentView.swift
@@ -8,10 +8,23 @@
 //  */
 
 import SwiftUI
+import UserNotifications
 
 class NotificationManager {
     
     static let instance = NotificationManager() //Singleton
+    
+    func requestAuthorization() {
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: options) { (success, error)  in
+            if let error = error {
+                print("Error \(error)")
+            } else {
+                print("Success")
+            }
+        }
+    }
 }
 
 struct ContentView: View {
