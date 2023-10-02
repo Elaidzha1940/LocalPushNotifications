f4a64fb7e880d21fe2c27c67a75d900f53378f5c
Elaidzha1940 <eli080293@gmail.com>
2023-10-02 04:54:40 +0300

    LocalPushNotifications

---
+++ LocalPush Notifications/LocalPushNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalPushNotificationsApp.swift
+//  LocalPush Notifications
+//
+//  Created by Elaidzha Shchukin on 02.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct LocalPush_NotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
